d0666676ab5a613895b3404627688584a819c2f9
Théo FIDRY <theo.fidry@gmail.com>
2017-01-06 18:31:22 +0000

    Add profile for Symfony

---
+++ symfony.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types = 1);
+
+use DiContainerBenchmarks\Container\Symfony\Resource\CompiledPrototypeContainer;
+use DiContainerBenchmarks\Container\Symfony\Resource\CompiledSingletonContainer;
+use DiContainerBenchmarks\Fixture\Class100;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
+
+require __DIR__.'/vendor/autoload.php';
+
+build();
+
+profile('Testsuite #2', CompiledPrototypeContainer::class);
+profile('Testsuite #4', CompiledSingletonContainer::class);
+
+
+function profile(string $title, string $containerClass)
+{
+    $blackfire = new \Blackfire\Client();
+
+    $config = new \Blackfire\Profile\Configuration();
+    $config->setTitle($title);
+    $config->setSamples(100);
+    $config->setReference(0);
+
+    $probe = $blackfire->createProbe($config, false);
+    for ($i = 1; $i <= $config->getSamples(); $i++) {
+        $probe->enable();
+
+        $container = new $containerClass();
+        $container->get(Class100::class);
+
+        $probe->close();
+    }
+    $blackfire->endProbe($probe);
+}
+
+function build()
+{
+    // Build container with prototype services
+    $containerBuilder = new ContainerBuilder();
+
+    for ($i = 1; $i <= 100; $i++) {
+        $definition = new Definition('DiContainerBenchmarks\Fixture\Class' . $i, []);
+        $definition->setShared(false);
+        $definition->setAutowired(true);
+        $containerBuilder->setDefinition('DiContainerBenchmarks\Fixture\Class' . $i, $definition);
+    }
+
+    $containerBuilder->compile();
+
+    $dumper = new PhpDumper($containerBuilder);
+    file_put_contents(
+        __DIR__."/src/Container/Symfony/Resource/CompiledPrototypeContainer.php",
+        $dumper->dump(
+            [
+                "namespace" => "DiContainerBenchmarks\\Container\\Symfony\\Resource",
+                "class" => "CompiledPrototypeContainer"
+            ]
+        )
+    );
+
+    // Build container with singleton services
+    $containerBuilder = new ContainerBuilder();
+
+    for ($i = 1; $i <= 100; $i++) {
+        $definition = new Definition('DiContainerBenchmarks\Fixture\Class' . $i, []);
+        $definition->setShared(true);
+        $definition->setAutowired(true);
+        $containerBuilder->setDefinition('DiContainerBenchmarks\Fixture\Class' . $i, $definition);
+    }
+
+    $containerBuilder->compile();
+
+    $dumper = new PhpDumper($containerBuilder);
+    file_put_contents(
+        __DIR__."/src/Container/Symfony/Resource/CompiledSingletonContainer.php",
+        $dumper->dump(
+            [
+                "namespace" => "DiContainerBenchmarks\\Container\\Symfony\\Resource",
+                "class" => "CompiledSingletonContainer"
+            ]
+        )
+    );
+}
